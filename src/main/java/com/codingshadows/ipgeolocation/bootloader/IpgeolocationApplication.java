16171276fdce5a710ea5c5620e1dbe770f936476
Rusu Dinu <dinu@codingshadows.com>
2021-12-31 12:48:27 +0200

    enabled scheduling

--- src/main/java/com/codingshadows/ipgeolocation/bootloader/IpgeolocationApplication.java
+++ src/main/java/com/codingshadows/ipgeolocation/bootloader/IpgeolocationApplication.java
@@ -2,12 +2,14 @@ package com.codingshadows.ipgeolocation.bootloader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class IpgeolocationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IpgeolocationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IpgeolocationApplication.class, args);
+    }
 
 }
