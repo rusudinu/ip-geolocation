6b0d29f0d630b4abe8d411447ef582bb10709e86
Rusu Dinu <dinu@codingshadows.com>
2021-12-31 13:44:56 +0200

    fixed controller returning empty: false instead of the geolocation data

--- src/main/java/com/codingshadows/ipgeolocation/controller/GeoDataController.java
+++ src/main/java/com/codingshadows/ipgeolocation/controller/GeoDataController.java
@@ -1,12 +1,15 @@
 package com.codingshadows.ipgeolocation.controller;
 
 import com.codingshadows.ipgeolocation.service.GeoDataService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("api")
@@ -15,7 +18,7 @@ public class GeoDataController {
     private final GeoDataService geoDataService;
 
     @GetMapping("get/{ip}")
-    public Object getGeoData(@PathVariable String ip) {
-        return geoDataService.getGeoData(ip);
+    public Map getGeoData(@PathVariable String ip) throws JsonProcessingException {
+        return new ObjectMapper().readValue(geoDataService.getGeoData(ip).toString(), Map.class);
     }
 }
