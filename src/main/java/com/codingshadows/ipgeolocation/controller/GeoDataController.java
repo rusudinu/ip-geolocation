16c798ff58d127dbb02f23489168525c7d5ef882
Rusu Dinu <dinu@codingshadows.com>
2021-12-31 15:43:28 +0200

    validating the ip

--- src/main/java/com/codingshadows/ipgeolocation/controller/GeoDataController.java
+++ src/main/java/com/codingshadows/ipgeolocation/controller/GeoDataController.java
@@ -4,13 +4,13 @@ import com.codingshadows.ipgeolocation.service.GeoDataService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Pattern;
 import java.util.Map;
 
+@Validated
 @RestController
 @RequestMapping("api")
 @RequiredArgsConstructor
@@ -18,7 +18,9 @@ public class GeoDataController {
     private final GeoDataService geoDataService;
 
     @GetMapping("get/{ip}")
-    public Map getGeoData(@PathVariable String ip) throws JsonProcessingException {
+    public Map getGeoData(@PathVariable @Pattern(regexp = "^(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)(\\.(25[0-5]|2[0-4]\\d|[0-1]?\\d?\\d)){3}$") String ip) throws JsonProcessingException {
+        if (ip == null || ip.equals("null"))
+            return null;
         return new ObjectMapper().readValue(geoDataService.getGeoData(ip).toString(), Map.class);
     }
 }
